1a5deea83f1b013db59083d2a9395df8a952b123
Alexandre FRANCOIS <afrancoi@student.42.fr>
2018-11-17 03:33:57 +0100

    GNL file

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/17 03:06:43 by afrancoi          #+#    #+#             */
+/*   Updated: 2018/11/17 03:32:38 by afrancoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 0
+
+int		get_next_line(const int fd, char **line);
+
+#endif
