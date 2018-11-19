f5899c506f7a2705a9a9e3633fc3802a1334b4c5
Alexandre FRANCOIS <afrancoi@student.42.fr>
2018-11-19 06:11:39 +0100

    GNL advancement

--- get_next_line.c
+++ get_next_line.c
@@ -6,35 +6,53 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 03:06:45 by afrancoi          #+#    #+#             */
-/*   Updated: 2018/11/18 03:34:10 by afrancoi         ###   ########.fr       */
+/*   Updated: 2018/11/19 06:11:08 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
 
+static int	ft_read(int fd, char **save)
+{
+	int		rd;
+	char	buffer[BUFF_SIZE + 1];
 
-/*
- **	- Mettre le buffer dans la static en enlevant la ligne envoyee
- **	- gerer les cas derreur de READ()
- **	- Avancer dans les lignes a chaques appel de la fonction.
- **	- gerer plusieurs FD ?
- **/
+	if (!*save)
+		if (!(*save = (char*)malloc(sizeof(char) * BUFF_SIZE + 1)))
+			return (-1);
+	while ((rd = read(fd, buffer, BUFF_SIZE)))
+	{
+		buffer[rd] = '\0';
+		if (!(*save = ft_strjoin(*save, buffer)))
+			return (-1);
+	}
+	return (1);
+}
 
 int		get_next_line(const int fd, char **line)
 {
-	static char	*str;
-	char		buffer[BUFF_SIZE];
-	int			rd;
-	char		*nl;
+	static	char	*save;
+	int				i;
 
-	while ((rd = read(fd, buffer, BUFF_SIZE)))
+	if (!fd || fd < 0 || !line)
+		return (-1);
+	if (!ft_read(fd, &save))
+		return (-1);
+	if (*save)
 	{
-		nl = ft_strchr(buffer, '\n');
-		if (nl)
-			break ;
+		i = 0;
+		while (save[i] != '\n' && save[i])
+			i++;
+		if (i > 0)
+		{
+			*line = ft_strsub(save, 0, i);
+			save += i + 1;
+		}
+		else
+			*line = ft_strdup("");
+		return (1);
 	}
+	else
+		*line = ft_strdup("");
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 03:06:43 by afrancoi          #+#    #+#             */
-/*   Updated: 2018/11/17 04:23:46 by afrancoi         ###   ########.fr       */
+/*   Updated: 2018/11/19 06:10:01 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <string.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
 
 # define BUFF_SIZE 30
 
